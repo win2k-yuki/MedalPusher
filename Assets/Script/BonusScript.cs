93fdd23ec8cf6e0af03a3ffde4134ab21edb1d30
win2k-yuki <aadolfhitlersiegheill@gmail.com>
2015-09-28 22:01:53 +0900

    メダルスポーンとかいろいろ実装

---
+++ Assets/Script/BonusScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BonusScript : MonoBehaviour {
+    //X軸をずらす +5から-5
+	// Use this for initialization
+    bool PlusEnable = true;
+    bool MinusEnable = false;
+    Vector3 initPosition;
+    Vector3 newPosition;
+    float fixedPosition = 0f;
+	void Start () {
+        initPosition = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (PlusEnable == true)
+        {
+            fixedPosition += 0.05f;
+            if (fixedPosition >= 5)
+            {
+                PlusEnable = false;
+                MinusEnable = true;
+            }
+        }
+        if (MinusEnable == true)
+        {
+            fixedPosition -= 0.05f;
+            if (fixedPosition <= -5)
+            {
+                PlusEnable = true;
+                MinusEnable = false;
+            }
+        }
+        newPosition = new Vector3 (initPosition.x + fixedPosition,
+                                   initPosition.y,
+                                   initPosition.z);
+        this.GetComponent<Rigidbody>().MovePosition(newPosition);
+	}
+}
---
+++ Assets/Script/SpawnerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerScript : MonoBehaviour {
+    public GameObject coin;
+    float moveSpeed = 8.0f;
+
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(x, 0, 0);
+        rb.velocity = direction * moveSpeed;
+        if (Input.GetKeyDown("space"))
+        {
+            Instantiate(coin, this.transform.position, this.transform.rotation);
+        }
+	}
+}
