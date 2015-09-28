7b8c15e47fc993eb10995c8f35e1c457874be3fe
win2k-yuki <aadolfhitlersiegheill@gmail.com>
2015-09-28 19:58:07 +0900

    プッシャースクリプトを追加

---
+++ Assets/Script/PusherScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PusherScript : MonoBehaviour {
+    //Z軸プラス方向でプッシャーを引っ込める
+	// Use this for initialization
+    Vector3 initPosition;
+    Vector3 newPosition;
+	void Start () {
+        initPosition = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        newPosition = new Vector3(initPosition.x,
+                                   initPosition.y,
+                                   initPosition.z + Mathf.Sin(Time.time)*2);
+        this.GetComponent<Rigidbody>().MovePosition(newPosition);
+	}
+}
