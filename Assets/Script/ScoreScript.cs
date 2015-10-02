2b93695d8ed13853263739e3b8d45dccbecf0e92
win2k-yuki <aadolfhitlersiegheill@gmail.com>
2015-10-02 16:40:51 +0900

    スコア減少を実装

---
+++ Assets/Script/ScoreScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ScoreScript : MonoBehaviour {
+    public int initScore;
+    int currentScore;
+    Text scoreText;
+	// Use this for initialization
+	void Start () {
+        currentScore = initScore;
+        scoreText = this.GetComponent<Text>();
+        printScore(initScore);
+
+	}
+    public void subScore(int n){
+        currentScore -= n;
+        printScore(currentScore);
+        
+    }
+    public void addScore(int n){
+        currentScore += n;
+        printScore(currentScore);
+    }
+    void printScore(int n){
+        scoreText.text = "Coin:" + n.ToString();
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Script/SpawnerScript.cs
+++ Assets/Script/SpawnerScript.cs
@@ -2,29 +2,44 @@
 using System.Collections;
 
 public class SpawnerScript : MonoBehaviour {
+    float moveSpeed = 8.0f;
+    Rigidbody rb;
+
     public GameObject coin;
+
     public GameObject Leftwall;
     public GameObject Rightwall;
-    float leftbrockerPositionX;
-    float rightbrockerPositionx;
-    float moveSpeed = 8.0f;
 
-    Rigidbody rb;
-	// Use this for initialization
+    float leftbrockerPositionX;
+    float rightbrockerPositionX;
+
+    public GameObject scoreText;
+    ScoreScript scoreS;
+
+   	// Use this for initialization
 	void Start () {
         rb = this.GetComponent<Rigidbody>();
         leftbrockerPositionX = Leftwall.transform.position.x;
         rightbrockerPositionX = Rightwall.transform.position.x;
+        scoreS = scoreText.GetComponent<ScoreScript>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Vector3 currentPosition = this.transform.position;
+        currentPosition.x = Mathf.Clamp(currentPosition.x,
+                                        leftbrockerPositionX,
+                                        rightbrockerPositionX);
+        this.transform.position = currentPosition;
+        //キー入力
         float x = Input.GetAxis("Horizontal");
+        //向き
         Vector3 direction = new Vector3(x, 0, 0);
         rb.velocity = direction * moveSpeed;
         if (Input.GetKeyDown("space"))
         {
             Instantiate(coin, this.transform.position, this.transform.rotation);
+            scoreS.subScore(1);
         }
 	}
 }
